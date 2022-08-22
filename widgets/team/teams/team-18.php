cf2b2acffe14223f4d8c57cb0c6de120bd8213ef
Md Abdul Kader <kader3b@gmail.com>
2022-08-22 13:51:26 +0600

    team-18 update
    team-18 update

--- widgets/team/teams/team-18.php
+++ widgets/team/teams/team-18.php
@@ -9,15 +9,13 @@ $team_style = $settings['team_style'];
             <div class="pic">
                 <img src="<?php echo esc_url($settings['team_image']['url']) ?>" alt="Image"/>
             </div>
+            <ul class="social-link">
+                <li><a href="#" class="fab fa-facebook"></a></li>
+                <li><a href="#" class="fab fa-google"></a></li>
+                <li><a href="#" class="fab fa-twitter"></a></li>
+            </ul>
             <div class="team-content">
                 <h3 class="post-title">Williamson<small>Web Developer</small></h3>
-                <ul class="social-link">
-                    <li><a href="#" class="fab fa-facebook"></a></li>
-                    <li><a href="#" class="fab fa-google"></a></li>
-                    <li><a href="#" class="fab fa-twitter"></a></li>
-                </ul>
-                <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum massa ac
-                    risus auctor venenatis. Pellentesque non. </p>
             </div>
         </div>
     </div>
